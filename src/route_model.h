2f48228f18d484e88942e941690e415eb99a1e5a
Arjun Vinod <arjun7965@gmail.com>
2019-05-31 17:47:16 -0400

    RouteModel: add necessary variables and methods

--- src/route_model.h
+++ src/route_model.h
@@ -12,10 +12,15 @@ class RouteModel : public Model {
     class Node : public Model::Node {
       public:
         // Add public Node variables and methods here.
-        
+        Node *parent = nullptr;
+        float h_value = std::numeric_limits<float>::max();
+        float g_value = 0.0f;
+        bool visited = false;
+        std::vector<Node*> neighbors;
+
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
-      
+
       private:
         // Add private Node variables and methods here.
         int index;
@@ -23,10 +28,12 @@ class RouteModel : public Model {
     };
     
     // Add public RouteModel variables and methods here.
-    RouteModel(const std::vector<std::byte> &xml);  
+    RouteModel(const std::vector<std::byte> &xml);
+    auto& SNodes() { return m_Nodes; }
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
 
   private:
     // Add private RouteModel variables and methods here.
+    std::vector<Node> m_Nodes;
 
 };
