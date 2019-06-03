f742939e2340e828fbe7acca27b1bd4fc0fc109d
Arjun Vinod <arjun7965@gmail.com>
2019-06-03 15:33:53 -0400

    RouteModel: add constructor which fills the m_Nodes vector

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -1,6 +1,11 @@
 #include "route_model.h"
 #include <iostream>
 
-RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
-
+RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml)
+{
+    int counter = 0;
+    for (Model::Node node : this->Nodes()) {
+        m_Nodes.push_back(Node(counter, this, node));
+        counter++;
+    }
 }
