7cceafe9b64c602d86a49796b6eb5bcfb9b2c0be
Arjun Vinod <arjun7965@gmail.com>
2019-06-13 18:31:42 -0400

    RoutePlanner: scale the start and end nodes, find the closest node to start and end node
        This is all done in the route_planner constructor

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,6 +1,17 @@
 #include "route_planner.h"
 #include <algorithm>
 
-RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
+RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model)
+{
+    // scale the floats to percentages
+    start_x *= 0.01f;
+    start_y *= 0.01f;
+    end_x *= 0.01f;
+    end_y *= 0.01f;
 
+    // find the closest nodes to (start_x, start_y)
+    start_node = &m_Model.FindClosestNode(start_x, start_y);
+
+    // find the closest nodes to (end_x, end_y)
+    end_node = &m_Model.FindClosestNode(end_x, end_y);
 }
